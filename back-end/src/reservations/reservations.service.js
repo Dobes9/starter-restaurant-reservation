859871bc47a0c3c18e69774bcccae02851ae6fba
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-11 12:49:20 -0700

    added create function, to make a new reservation in the database

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,10 +1,15 @@
 const knex = require("../db/connection");
 const tableName = "reservations";
 
-async function list(date) {
-  return knex(tableName).where({ reservation_date: date });
+async function list(reservation_date) {
+  return knex(tableName).where({ reservation_date });
+}
+
+function create(newReservation) {
+  return knex(tableName).insert(newReservation).returning("*");
 }
 
 module.exports = {
   list,
+  create,
 };
