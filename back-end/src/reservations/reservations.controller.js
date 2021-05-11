e47b72a295b337e925f9e9122f55462f60578863
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-11 11:21:19 -0700

    reworked date constant from request query

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,17 @@
 /**
  * List handler for reservation resources
  */
+
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 async function list(req, res) {
+  const date = req.query.date;
   res.json({
-    data: [],
+    data: await service.list(date),
   });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
