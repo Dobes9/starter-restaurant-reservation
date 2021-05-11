5c0b6b3422a3ac6b96dbd1da038fced8397206b8
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-11 08:46:45 -0700

    changed max capacity of party from 12 to 6

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -137,12 +137,6 @@ export default function ReservationForm() {
             <option value="4">Four</option>
             <option value="5">Five</option>
             <option value="6">Six</option>
-            <option value="7">Seven</option>
-            <option value="8">Eight</option>
-            <option value="9">Nine</option>
-            <option value="10">Ten</option>
-            <option value="11">Eleven</option>
-            <option value="12">Twelve</option>
           </select>
         </div>
       </div>
