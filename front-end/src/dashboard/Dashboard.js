5495fa2ee51b352b908c1d2a16f444f6463952e7
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-11 07:33:03 -0700

    imported useHistory, and previous, today, and next from utils/date-time, for buttons to change date

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { previous, today, next } from "../utils/date-time";
 
 /**
  * Defines the dashboard page.
@@ -27,7 +29,7 @@ function Dashboard({ date }) {
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for {date}</h4>
       </div>
       <ErrorAlert error={reservationsError} />
       {JSON.stringify(reservations)}
