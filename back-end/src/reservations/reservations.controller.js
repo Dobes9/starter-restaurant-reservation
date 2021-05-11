6855df56598ad5f21f3531fadb1457b868772918
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-11 12:50:07 -0700

    added create function, as well as data, first name, last name, mobile number, and people validation functions

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -12,6 +12,76 @@ async function list(req, res) {
   });
 }
 
+function hasData(req, res, next) {
+  if (req.body.data) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Body must have a data property`,
+  });
+}
+
+function hasFirstName(req, res, next) {
+  const { first_name } = req.body.data;
+  if (first_name) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Reservation must have a first name`,
+  });
+}
+
+function hasLastName(req, res, next) {
+  const { last_name } = req.body.data;
+  if (last_name) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Reservation must have a last name`,
+  });
+}
+
+function hasMobileNumber(req, res, next) {
+  const { mobile_number } = req.body.data;
+  if (mobile_number) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Reservation must have a mobile number`,
+  });
+}
+
+function hasNumOfPeople(req, res, next) {
+  const { people } = req.body.data;
+  if (people >= 1) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Reservation must have at least 1 person`,
+  });
+}
+
+async function create(req, res) {
+  const newReservation = await service.create(req.body.data);
+
+  res.status(201).json({
+    data: newReservation,
+  });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
+  create: [
+    hasData,
+    hasFirstName,
+    hasLastName,
+    hasMobileNumber,
+    hasNumOfPeople,
+    asyncErrorBoundary(create),
+  ],
 };
