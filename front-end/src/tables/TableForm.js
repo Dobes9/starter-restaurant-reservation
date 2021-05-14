5d775f9342fdccbd11c557c14dd3d3f7e071e9f1
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-14 11:21:57 -0700

    added fields on form, validation functions, error state to be passed into ErrorAlert component if errors occur

--- front-end/src/tables/TableForm.js
+++ front-end/src/tables/TableForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
 
 export default function TableForm() {
   const history = useHistory();
@@ -9,6 +10,7 @@ export default function TableForm() {
   };
 
   const [formData, setFormData] = useState({ ...initialFormData });
+  const [error, setError] = useState([]);
 
   const formChangeHandler = ({ target }) => {
     setFormData({
@@ -17,8 +19,32 @@ export default function TableForm() {
     });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (validateFields()) {
+      // API function to go here
+      history.push(`/dashboard`);
+    }
+  };
+
+  const validateFields = () => {
+    let foundError = null;
+
+    if (formData.table_name === "" || formData.capacity === "") {
+      foundError = { message: `Please fill out all fields.` };
+    } else if (formData.table_name.length < 2) {
+      foundError = { message: `Table name must be at least 2 characters` };
+    }
+    setError(foundError);
+
+    return foundError.length !== null;
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
+      <div>
+        <ErrorAlert error={error} />
+      </div>
       <div className="row mb-3">
         <div className="col-6 form-group">
           <label className="form-label" htmlFor="table_name">
@@ -38,11 +64,29 @@ export default function TableForm() {
       </div>
       <div className="row mb-3">
         <div className="col-6 form-group">
-            
+          <label className="form-label" htmlFor="capacity">
+            Table Capacity
+          </label>
+          <input
+            className="form-control"
+            id="capacity"
+            name="capacity"
+            type="number"
+            max="12"
+            min="1"
+            value={formData.capacity}
+            onChange={formChangeHandler}
+            required
+          />
         </div>
       </div>
       <div>
-        <button className="btn btn-secondary mx-1">Cancel</button>
+        <button
+          className="btn btn-secondary mx-1"
+          onClick={() => history.goBack()}
+        >
+          Cancel
+        </button>
         <button className="btn btn-primary mx-1" type="submit">
           Submit
         </button>
