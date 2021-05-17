895c362e47cfc2b5ec4b9c740514f61a53b25b8a
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-17 13:56:18 -0700

    added status td to table

--- front-end/src/tables/TablesDisplay.js
+++ front-end/src/tables/TablesDisplay.js
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function TablesDisplay({ tables }) {
   const listTables = tables.map((table) => {
-    const { table_id, table_name, capacity } = table;
+    const { table_id, table_name, capacity, status } = table;
 
     return (
       <tr key={table_id}>
         <td>{table_id}</td>
         <td>{table_name}</td>
         <td>{capacity}</td>
+        <td data-table-id-status={table_id}>{status}</td>
       </tr>
     );
   });
