c3a3879c27749b3670180a114165fadb21476ee6
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-17 19:53:58 -0700

    removed defaultTo method, as it was not working

--- back-end/src/db/migrations/20210517094436_createTables.js
+++ back-end/src/db/migrations/20210517094436_createTables.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.increments("table_id").primary();
     table.string("table_name");
     table.integer("capacity");
-    table.string("status").defaultTo("Free");
+    table.string("status");
     table.timestamps(true, true);
   });
 };
