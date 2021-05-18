46f7625220f8290d11b439e4f44c28edd0be5379
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-18 08:29:24 -0700

    list and create functions make use of service functions

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,9 +1,17 @@
 const service = require("./tables.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function list(req, res) {}
+async function list(req, res) {
+  res.json({ data: await service.list() });
+}
 
-async function create(req, res) {}
+async function create(req, res) {
+  const newTable = await service.create(req.body.data);
+
+  res.status(201).json({
+    data: newTable,
+  });
+}
 
 module.exports = {
   list: asyncErrorBoundary(list),
