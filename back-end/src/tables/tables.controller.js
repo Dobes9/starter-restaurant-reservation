777d6f25c94290b4a403b41ce7017d34dbaeedb0
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 11:18:36 -0700

    added hasData, tableNameHasAtLeast2Chars, and tableCanSeatAtLeastOnePerson validation functions, exported as part of create

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -5,6 +5,36 @@ async function list(req, res) {
   res.json({ data: await service.list() });
 }
 
+function hasData(req, res, next) {
+  if (req.body.data) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Body must have a data property.`,
+  });
+}
+
+function tableNameHasAtLeast2Chars(req, res, next) {
+  if (req.body.data.table_name.length >= 2) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Table name must be at least 2 characters long.`,
+  });
+}
+
+function tableCanSeatAtLeastOnePerson(req, res, next) {
+  if (req.body.data.capacity >= 1) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Table must have a capacity of at least 1.`,
+  });
+}
+
 async function create(req, res) {
   const newTable = await service.create(req.body.data);
 
@@ -17,6 +47,11 @@ async function update(req, res) {}
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [asyncErrorBoundary(create)],
+  create: [
+    hasData,
+    tableNameHasAtLeast2Chars,
+    tableCanSeatAtLeastOnePerson,
+    asyncErrorBoundary(create),
+  ],
   update: [asyncErrorBoundary(update)],
 };
