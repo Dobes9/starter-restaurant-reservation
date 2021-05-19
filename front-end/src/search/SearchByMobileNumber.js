577b9a0db90df1c308c42da46c9f7ebe016aa7d8
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 13:24:39 -0700

    basic shape of search form

--- front-end/src/search/SearchByMobileNumber.js
+++ front-end/src/search/SearchByMobileNumber.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import ListReservations from "../reservations/ListReservations";
 
 export default function SearchByMobileNumber() {
-  return <h4>Placeholder text</h4>;
+  const [formData, setFormData] = useState(null);
+
+  const formChangeHandler = ({ target }) => {
+    setFormData(target.value);
+  };
+
+  return (
+    <>
+      <form>
+        <div className="row mt-3">
+          <div className="col-6 form-group">
+            <input
+              className="form-control"
+              id="mobile_number"
+              name="mobile_number"
+              type="search"
+              value={formData}
+              onChange={formChangeHandler}
+              placeholder="Enter a customer's phone number"
+              required
+            />
+          </div>
+          <div className="col-6 form-group">
+            <button className="btn btn-primary" type="submit">
+              Find
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
