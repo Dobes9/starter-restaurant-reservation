f8659b5d8f2b3e97daf6e77e022f931c69e30cd7
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 11:36:30 -0700

    added read function, exported read function

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -9,7 +9,12 @@ function create(newReservation) {
   return knex(tableName).insert(newReservation).returning("*");
 }
 
+async function read(reservation_id) {
+  return knex(tableName).where({ reservation_id }).first();
+}
+
 module.exports = {
   list,
   create,
+  read,
 };
