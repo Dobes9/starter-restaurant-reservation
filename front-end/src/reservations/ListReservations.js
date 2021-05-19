a8c10f695b917cf713a6553c53cff98cdadbdd7b
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 13:04:02 -0700

    initial commit, moved contents from Dashboard component

---
+++ front-end/src/reservations/ListReservations.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ErrorAlert from "../layout/ErrorAlert";
+import ReservationDisplay from "./ReservationDisplay";
+
+export default function listReservations({ reservations, reservationsError }) {
+  return (
+    <>
+      <ErrorAlert error={reservationsError} />
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Mobile Number</th>
+            <th scope="col">Time</th>
+            <th scope="col">People</th>
+            <th scope="col">Status</th>
+            <th scope="col">Seat Table</th>
+            <th scope="col">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ReservationDisplay reservations={reservations} />
+        </tbody>
+      </table>
+    </>
+  );
+}
