d955f8d987502b94daccf2b1e96a42f074dc190b
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 12:03:02 -0700

    created update function, exported update

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -13,8 +13,16 @@ async function read(table_id) {
   return knex(tableName).where({ table_id }).first();
 }
 
+async function update(table) {
+  return knex(tableName)
+    .where({ table_id: table.table_id })
+    .update(table, "*")
+    .then(() => read(table.table_id));
+}
+
 module.exports = {
   list,
   create,
   read,
+  update,
 };
