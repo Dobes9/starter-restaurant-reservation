4dadf3b879a41ac3653bcf028a2e64e09809a6e0
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 13:04:36 -0700

    moved elements to display reservations to ListReservations component

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import ErrorAlert from "../layout/ErrorAlert";
 import { previous, today, next } from "../utils/date-time";
 import ReservationDisplay from "../reservations/ReservationDisplay";
 import TablesDisplay from "../tables/TablesDisplay";
+import ListReservations from "../reservations/ListReservations";
 
 /**
  * Defines the dashboard page.
@@ -26,25 +27,10 @@ function Dashboard({
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for {date}</h4>
       </div>
-      <ErrorAlert error={reservationsError} />
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Mobile Number</th>
-            <th scope="col">Time</th>
-            <th scope="col">People</th>
-            <th scope="col">Status</th>
-            <th scope="col">Seat Table</th>
-            <th scope="col">Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          <ReservationDisplay reservations={reservations} />
-        </tbody>
-      </table>
+      <ListReservations
+        reservations={reservations}
+        reservationsError={reservationsError}
+      />
 
       <h4>Tables</h4>
 
