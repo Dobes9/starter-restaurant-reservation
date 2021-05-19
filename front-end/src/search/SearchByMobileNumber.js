43e4eba3a8c3bfb9eabf832a373c89e5df34cf8c
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 14:36:02 -0700

    created listResults function, making use of listReservations API call, added to submitHandler

--- front-end/src/search/SearchByMobileNumber.js
+++ front-end/src/search/SearchByMobileNumber.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import ListReservations from "../reservations/ListReservations";
 import { listReservations } from "../utils/api";
+import useQuery from "../utils/useQuery";
 
 export default function SearchByMobileNumber() {
-  const [formData, setFormData] = useState(null);
+  const [mobile_number, setMobile_number] = useState(null);
   const [results, setResults] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
   const [displayResults, setDisplayResults] = useState(false);
 
   const formChangeHandler = ({ target }) => {
-    setFormData(target.value);
+    setMobile_number(target.value);
+  };
+
+  function listResults() {
+    const abortController = new AbortController();
+    listReservations({ mobile_number }, abortController.signal)
+      .then(setResults)
+      .catch(setReservationsError);
+    return () => abortController.abort();
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    listResults();
+    setDisplayResults(true);
   };
 
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="row mt-3">
           <div className="col-6 form-group">
             <input
@@ -21,7 +37,7 @@ export default function SearchByMobileNumber() {
               id="mobile_number"
               name="mobile_number"
               type="search"
-              value={formData}
+              value={mobile_number}
               onChange={formChangeHandler}
               placeholder="Enter a customer's phone number"
               required
@@ -36,7 +52,10 @@ export default function SearchByMobileNumber() {
       </form>
       {displayResults ? (
         results.length ? (
-          <ListReservations reservations={results} />
+          <ListReservations
+            reservations={results}
+            reservationsError={reservationsError}
+          />
         ) : (
           <h4>No resevations found</h4>
         )
