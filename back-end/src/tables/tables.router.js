b69f22549755cb694e70df2bddb54ae7bed2f98a
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-19 09:38:19 -0700

    added delete method to :table_id/seat rout

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const controller = require("./tables.controller");
 
 router.route("/").get(controller.list).post(controller.create);
-router.route("/:table_id/seat").put();
+router.route("/:table_id/seat").put().delete();
 
 module.exports = router;
