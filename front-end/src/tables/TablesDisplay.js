7f20bb41d8ae5a3dc0bf276be2cc647b88d022bc
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-21 14:24:31 -0700

    added finish button that displays when table status is occupied, starting to define what button does on click

--- front-end/src/tables/TablesDisplay.js
+++ front-end/src/tables/TablesDisplay.js
@@ -4,12 +4,36 @@ export default function TablesDisplay({ tables }) {
   const allTables = tables.map((table) => {
     const { table_id, table_name, capacity, status } = table;
 
+    const clickHandler = (event) => {
+      event.preventDefault();
+      // API call to go here
+    };
+
     return (
       <tr key={table_id}>
         <td>{table_id}</td>
         <td>{table_name}</td>
         <td>{capacity}</td>
         <td data-table-id-status={table_id}>{status}</td>
+        <td>
+          {status === "occupied" ? (
+            <button
+              className="btn btn-primary"
+              type="button"
+              data-table-id-finish={table_id}
+              onClick={() => {
+                const confirmation = window.confirm(
+                  `Is this table ready to seat new guests? This cannot be undone.`
+                );
+                if (confirmation) {
+                  clickHandler();
+                }
+              }}
+            >
+              Finish
+            </button>
+          ) : null}
+        </td>
       </tr>
     );
   });
