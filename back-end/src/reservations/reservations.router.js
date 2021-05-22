dbd5816fa680d23fd004a1322210b68c20db61e9
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-21 17:34:44 -0700

    added put method to route /reservations/:reservation_id/status

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,5 +9,6 @@ const controller = require("./reservations.controller");
 
 router.route("/").get(controller.list).post(controller.create);
 router.route("/:reservation_id").get(controller.read).put();
+router.route("/:reservation_id/status").put();
 
 module.exports = router;
