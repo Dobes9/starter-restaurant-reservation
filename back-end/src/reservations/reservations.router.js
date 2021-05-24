ccb2ceb000e5c3046922c07ae1e926cf2cbbca51
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-24 08:04:37 -0700

    mounted updateStatus function on put method for route /reservations/:reservation_id/status

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,6 +9,6 @@ const controller = require("./reservations.controller");
 
 router.route("/").get(controller.list).post(controller.create);
 router.route("/:reservation_id").get(controller.read).put();
-router.route("/:reservation_id/status").put();
+router.route("/:reservation_id/status").put(controller.updateStatus);
 
 module.exports = router;
