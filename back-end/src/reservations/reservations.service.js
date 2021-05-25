9b03554b36756e65f6c91a714c779937697ea001
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-25 08:24:41 -0700

    added orderBy method to list function, to display the reservations by their time

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,7 +2,9 @@ const knex = require("../db/connection");
 const tableName = "reservations";
 
 async function list(reservation_date) {
-  return knex(tableName).where({ reservation_date });
+  return knex(tableName)
+    .where({ reservation_date })
+    .orderBy("reservation_time");
 }
 
 function create(newReservation) {
