7697dccd951c1378e13d51132871636f500dc990
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-25 09:15:47 -0700

    utilizing search props passed to this component, added filter method to listReservations, if search is true, all reservations pass, if search is false, only reservations that are not finished pass

--- front-end/src/reservations/ReservationsDisplay.js
+++ front-end/src/reservations/ReservationsDisplay.js
@@ -6,79 +6,84 @@ import ErrorAlert from "../layout/ErrorAlert";
 export default function ReservationsDisplay({
   reservations,
   reservationsError,
+  search,
 }) {
   const history = useHistory();
   const abortController = new AbortController();
 
-  const listReservations = reservations.map((reservation) => {
-    const {
-      reservation_id,
-      first_name,
-      last_name,
-      mobile_number,
-      reservation_date,
-      reservation_time,
-      people,
-      status,
-    } = reservation;
-    const readableTime = new Date(
-      `${reservation_date}T${reservation_time}`
-    ).toLocaleTimeString();
+  const listReservations = reservations
+    .filter((reservation) => {
+      return search ? true : reservation.status !== "finished";
+    })
+    .map((reservation) => {
+      const {
+        reservation_id,
+        first_name,
+        last_name,
+        mobile_number,
+        reservation_date,
+        reservation_time,
+        people,
+        status,
+      } = reservation;
+      const readableTime = new Date(
+        `${reservation_date}T${reservation_time}`
+      ).toLocaleTimeString();
 
-    return (
-      <tr key={reservation_id}>
-        <td>{reservation_id}</td>
-        <td>{first_name}</td>
-        <td>{last_name}</td>
-        <td>{mobile_number}</td>
-        <td>{readableTime}</td>
-        <td>{people}</td>
-        <td data-reservation-id-status={reservation_id}>{status}</td>
-        <td>
-          {status === "booked" ? (
-            <a href={`/reservations/${reservation_id}/seat`}>
-              <button className="btn btn-primary" type="button">
-                Seat
-              </button>
-            </a>
-          ) : null}
-        </td>
-        <td>
-          {status === "booked" ? (
-            <a href={`/reservations/${reservation_id}/edit`}>
-              <button className="btn btn-primary" type="button">
-                Edit
-              </button>
-            </a>
-          ) : null}
-        </td>
-        <td>
-          {status === "booked" ? (
-            <button
-              className="btn btn-danger"
-              type="button"
-              data-reservation-id-cancel={reservation_id}
-              onClick={() => {
-                const confirmation = window.confirm(
-                  `Do you want to cancel this reservation? This cannot be undone.`
-                );
-                if (confirmation) {
-                  changeReservationStatus(
-                    reservation_id,
-                    "cancelled",
-                    abortController.signal
+      return (
+        <tr key={reservation_id}>
+          <td>{reservation_id}</td>
+          <td>{first_name}</td>
+          <td>{last_name}</td>
+          <td>{mobile_number}</td>
+          <td>{readableTime}</td>
+          <td>{people}</td>
+          <td data-reservation-id-status={reservation_id}>{status}</td>
+          <td>
+            {status === "booked" ? (
+              <a href={`/reservations/${reservation_id}/seat`}>
+                <button className="btn btn-primary" type="button">
+                  Seat
+                </button>
+              </a>
+            ) : null}
+          </td>
+          <td>
+            {status === "booked" ? (
+              <a href={`/reservations/${reservation_id}/edit`}>
+                <button className="btn btn-primary" type="button">
+                  Edit
+                </button>
+              </a>
+            ) : null}
+          </td>
+          <td>
+            {status === "booked" ? (
+              <button
+                className="btn btn-danger"
+                type="button"
+                data-reservation-id-cancel={reservation_id}
+                onClick={() => {
+                  const confirmation = window.confirm(
+                    `Do you want to cancel this reservation? This cannot be undone.`
                   );
-                  history.go(0);
-                }
-              }}
-            >
-              Cancel
-            </button>
-          ) : null}
-        </td>
-      </tr>
-    );
-  });
+                  if (confirmation) {
+                    changeReservationStatus(
+                      reservation_id,
+                      "cancelled",
+                      abortController.signal
+                    );
+                    history.go(0);
+                  }
+                }}
+              >
+                Cancel
+              </button>
+            ) : null}
+          </td>
+        </tr>
+      );
+    });
 
   return (
     <>
