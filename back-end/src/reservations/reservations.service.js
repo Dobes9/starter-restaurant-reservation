14a301c7f15382433542e2bfc708c66cad1e2d93
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-27 14:00:22 -0700

    added whereNot method for list function, currently commented out, as having it passes some tests, and fails some others

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,6 +4,7 @@ const tableName = "reservations";
 async function list(reservation_date) {
   return knex(tableName)
     .where({ reservation_date })
+    //.whereNot({ status: "finished" })
     .orderBy("reservation_time");
 }
 
