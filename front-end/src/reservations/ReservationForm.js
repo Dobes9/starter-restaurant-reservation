ba74dc8a99e54472f3c2f26f609f5c6d1b4269af
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-27 19:25:47 -0700

    removed pattern from mobile_number input, changed input type to text

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -211,9 +211,9 @@ export default function ReservationForm({
               className="form-control"
               id="mobile_number"
               name="mobile_number"
-              type="tel"
+              type="text"
               placeholder="XXX-XXX-XXXX"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              //pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               value={reservation.mobile_number}
               onChange={changeHandler}
               required
