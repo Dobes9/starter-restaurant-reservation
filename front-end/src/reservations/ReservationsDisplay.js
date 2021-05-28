15488fc7e543850f9e267394d4536e3d8e2201ea
Richard H Dobeck <rdobeck@yahoo.com>
2021-05-27 17:39:47 -0700

    removed filter method on listReservations, as it is now unnecessary with changes made to reservations service

--- front-end/src/reservations/ReservationsDisplay.js
+++ front-end/src/reservations/ReservationsDisplay.js
@@ -11,82 +11,78 @@ export default function ReservationsDisplay({
   const history = useHistory();
   const abortController = new AbortController();
 
-  const listReservations = reservations
-    .filter((reservation) => {
-      return search ? true : ["booked", "seated"].includes(reservation.status);
-    })
-    .map((reservation) => {
-      const {
+  const listReservations = reservations.map((reservation) => {
+    const {
+      reservation_id,
+      first_name,
+      last_name,
+      mobile_number,
+      reservation_date,
+      reservation_time,
+      people,
+      status,
+    } = reservation;
+    const readableTime = new Date(
+      `${reservation_date}T${reservation_time}`
+    ).toLocaleTimeString();
+
+    const handleCancelReservation = async () => {
+      await changeReservationStatus(
         reservation_id,
-        first_name,
-        last_name,
-        mobile_number,
-        reservation_date,
-        reservation_time,
-        people,
-        status,
-      } = reservation;
-      const readableTime = new Date(
-        `${reservation_date}T${reservation_time}`
-      ).toLocaleTimeString();
-
-      const handleCancelReservation = async () => {
-        await changeReservationStatus(
-          reservation_id,
-          "cancelled",
-          abortController.signal
-        );
-        history.go(0);
-        return () => abortController.abort();
-      };
-
-      return (
-        <tr key={reservation_id}>
-          <td>{reservation_id}</td>
-          <td>{first_name}</td>
-          <td>{last_name}</td>
-          <td>{mobile_number}</td>
-          <td>{readableTime}</td>
-          <td>{people}</td>
-          <td data-reservation-id-status={reservation_id}>{status}</td>
-          <td>
-            {status === "booked" ? (
-              <a href={`/reservations/${reservation_id}/seat`}>
-                <button className="btn btn-primary" type="button">
-                  Seat
-                </button>
-              </a>
-            ) : null}
-          </td>
-          <td>
-            {status === "booked" ? (
-              <a href={`/reservations/${reservation_id}/edit`}>
-                <button className="btn btn-primary" type="button">
-                  Edit
-                </button>
-              </a>
-            ) : null}
-          </td>
-          <td>
-            {status === "booked" ? (
-              <button
-                className="btn btn-danger"
-                type="button"
-                data-reservation-id-cancel={reservation_id}
-                onClick={() => {
-                  const confirmation = window.confirm(
-                    `Do you want to cancel this reservation? This cannot be undone.`
-                  );
-                  return confirmation ? handleCancelReservation() : null;
-                }}
-              >
-                Cancel
-              </button>
-            ) : null}
-          </td>
-        </tr>
+        "cancelled",
+        abortController.signal
       );
-    });
+      history.go(0);
+      return () => abortController.abort();
+    };
+
+    return (
+      <tr key={reservation_id}>
+        <td>{reservation_id}</td>
+        <td>{first_name}</td>
+        <td>{last_name}</td>
+        <td>{mobile_number}</td>
+        <td>{readableTime}</td>
+        <td>{people}</td>
+        <td data-reservation-id-status={reservation_id}>{status}</td>
+        <td>
+          {status === "booked" ? (
+            <a href={`/reservations/${reservation_id}/seat`}>
+              <button className="btn btn-primary" type="button">
+                Seat
+              </button>
+            </a>
+          ) : null}
+        </td>
+        <td>
+          {status === "booked" ? (
+            <a href={`/reservations/${reservation_id}/edit`}>
+              <button className="btn btn-primary" type="button">
+                Edit
+              </button>
+            </a>
+          ) : null}
+        </td>
+        <td>
+          {status === "booked" ? (
+            <button
+              className="btn btn-danger"
+              type="button"
+              data-reservation-id-cancel={reservation_id}
+              onClick={() => {
+                const confirmation = window.confirm(
+                  `Do you want to cancel this reservation? This cannot be undone.`
+                );
+                return confirmation ? handleCancelReservation() : null;
+              }}
+            >
+              Cancel
+            </button>
+          ) : null}
+        </td>
+      </tr>
+    );
+  });
 
   return (
     <>
